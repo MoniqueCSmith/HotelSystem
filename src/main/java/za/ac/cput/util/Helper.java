a70ae76da0c942e1ae2b6ef8ad33fb6b1b9f1ff1
MoniqueCSmith <217041701@mycput.ac.za>
2023-04-09 21:30:45 +0200

    Reservation and ReservationDate completed.

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,4 +1,14 @@
 package za.ac.cput.util;
 
+import java.util.UUID;
+
 public class Helper {
+    public static boolean isNullorEmpty(String s) {
+        return (s == null || s.equals("") || s.isEmpty() || s.equalsIgnoreCase("null")) ;
+    }
+   public static String reservationID(){
+        return UUID.randomUUID().toString();
+
+}
+
 }

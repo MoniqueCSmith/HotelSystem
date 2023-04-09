db01aa719facbeab684ecffbf1a271a17052b5b8
MoniqueCSmith <217041701@mycput.ac.za>
2023-04-09 19:55:32 +0200

    Domain, Factory, Repository, and Test classes for MealPlan and DietaryRequirement.

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,4 +1,14 @@
 package za.ac.cput.util;
 
+import java.util.UUID;
+
 public class Helper {
+
+    public static String generateID() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static boolean isNullOrEmpty (String s) {
+        return (s == null || s.equals("") || s.isEmpty() || s.equalsIgnoreCase("null"));
+    }
 }

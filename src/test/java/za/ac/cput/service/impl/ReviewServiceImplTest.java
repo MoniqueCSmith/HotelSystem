8a6f36b45261ae4fa1cea6731a071c85f029e7ea
ShanlynnC <shanlynncourtney@gmail.com>
2023-06-10 22:16:07 +0200

    Service for Membership and Review completed.

--- src/test/java/za/ac/cput/service/impl/ReviewServiceImplTest.java
+++ src/test/java/za/ac/cput/service/impl/ReviewServiceImplTest.java
@@ -1,10 +1,11 @@
 /**
- * ReviewRepositoryTest.java
- * Repository Test class for the Review
+ * ReviewServiceImplTest.java
+ * Service Test class for the Review
  * Author: Shanlynn Courtney Thomas (218053762)
- * Date: 08 April 2023
+ * Date: 10 June 2023
  */
-package za.ac.cput.repository;
+
+package za.ac.cput.service.impl;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -15,35 +16,34 @@ import za.ac.cput.factory.ReviewFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
-class ReviewRepositoryTest {
-    private static ReviewRepository repository = ReviewRepository.getRepository();
-    private static Review review = ReviewFactory.buildReview(4, "Was a blissful experience!");
+public class ReviewServiceImplTest {
+    private static ReviewServiceImpl service = ReviewServiceImpl.getService();
+    private static Review review = ReviewFactory.buildReview(4,"Was the best experience ever!");
 
     @Test
     void a_create() {
-        Review created = repository.create(review);
+        Review created = service.create(review);
         assertEquals(review.getReviewID(), created.getReviewID());
         System.out.println("Create:" + created);
     }
 
     @Test
     void b_read() {
-        Review read = repository.read(review.getReviewID());
+        Review read = service.read(review);
         assertNotNull(read);
         System.out.println("Read:" + read);
     }
 
-
     @Test
     void c_delete() {
-        boolean success = repository.delete(review.getReviewID());
+        boolean success = service.delete(review);
         assertTrue(success);
-        System.out.println("Deleted" + success);
+        System.out.println("Deleted:" + success);
     }
 
     @Test
     void d_getAll() {
         System.out.println("Show all:");
-        System.out.println(repository.getAll());
+        System.out.println(service.getAll());
     }
 }

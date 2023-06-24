bf633dac1db727bff492617a332f2232d5de4d58
Alison <219140987@mycput.ac.za>
2023-06-24 11:56:47 +0200

    changes made to Amenity and Room, addition of Annotations and services tests as well. It works when I test it separately, I also changed my helper to work for now. After exams I will properly change it.

--- src/main/java/za/ac/cput/service/impl/RoomServiceImpl.java
+++ src/main/java/za/ac/cput/service/impl/RoomServiceImpl.java
@@ -7,51 +7,52 @@
 package za.ac.cput.service.impl;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Room;
-import za.ac.cput.repository.impl.RoomRepositoryImpl;
+import za.ac.cput.repository.IRoomRepository;
 import za.ac.cput.service.RoomService;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class RoomServiceImpl implements RoomService {
-    private static RoomServiceImpl service = null;
-    private RoomRepositoryImpl repository = null;
+    private IRoomRepository repository;
 
-    private RoomServiceImpl(){
-        if(repository == null) {
-            repository = RoomRepositoryImpl.getRepository();
-        }
+    @Autowired
+    private RoomServiceImpl(IRoomRepository repository){
+        this.repository = repository;
     }
-    public static RoomServiceImpl getService() {
-        if (service == null) {
-            service = new RoomServiceImpl();
-        }
-        return service;
-    }
-
     @Override
     public Room create(Room room) {
-        return repository.create(room);
+        return this.repository.save(room);
     }
 
     @Override
-    public Room read(String id) {
-        return repository.read(id);
+    public Room read(String roomNo) {
+        return this.repository.findById(roomNo).orElse(null);
     }
 
     @Override
     public Room update(Room room) {
-        return repository.update(room);
+        if(this.repository.existsById(room.getRoomNo()))
+            return this.repository.save(room);
+        return null;
     }
 
     @Override
-    public boolean delete(String id) {
-        return repository.delete(id);
+    public boolean delete(String roomNo) {
+        if(this.repository.existsById(roomNo)) {
+            this.repository.deleteById(roomNo);
+            return true;
+        }
+        return false;
+
     }
+
     @Override
-    public Set<Room> getAll() {
-        return repository.getAll();
+    public List<Room> getAll() {
+        return this.repository.findAll();
     }
 }

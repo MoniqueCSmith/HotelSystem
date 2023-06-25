e7960ebbda9caff4237aafa52fcd18f3958a0f83
WilliamsAshton <127831002+WilliamsAshton@users.noreply.github.com>
2023-06-25 20:33:30 +0200

    Update InvoiceController.java

--- src/main/java/za/ac/cput/controller/InvoiceController.java
+++ src/main/java/za/ac/cput/controller/InvoiceController.java
@@ -8,7 +8,7 @@ package za.ac.cput.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import za.ac.cput.domain.Invoice;
-import za.ac.cput.util.factory.InvoiceFactory;
+import za.ac.cput.factory.InvoiceFactory;
 import za.ac.cput.service.InvoiceService;
 
 import java.util.List;

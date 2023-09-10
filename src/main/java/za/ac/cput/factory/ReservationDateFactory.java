e61cc3aa8b02558d448dd4de79b84fd50583ec2d
KYRA PETERSEN <102528904+KYRA219@users.noreply.github.com>
2023-09-10 19:26:57 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/factory/ReservationDateFactory.java
@@ -0,0 +1,43 @@
+/*
+ReservationDateFactory.java
+ReservationDate factory class
+Author : Kyra Petersen (219474559)
+Date : 5 June 2023
+*/
+
+
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.ReservationDate;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static za.ac.cput.util.Helper.isCheckInTimeValid;
+
+public class ReservationDateFactory {
+
+
+    public static ReservationDate buildReservationDate( LocalDate checkInDate , LocalDate checkOutDate , LocalDateTime estCheckInTime ){
+
+
+        if (
+                Helper.isNullOrEmpty(String.valueOf(checkInDate)) ||
+                Helper.isNullOrEmpty(String.valueOf(checkOutDate)) ||
+                Helper.isNullOrEmpty(String.valueOf(estCheckInTime))) return null;
+
+        if (!Helper.isDateRangeValid(checkInDate, checkOutDate)) {
+            return null;
+        }
+
+        if (!isCheckInTimeValid(estCheckInTime)) {
+            return null;
+        }
+        return new ReservationDate.Builder()
+                .setCheckInDate(checkInDate)
+                .setCheckOutDate(checkOutDate)
+                .setEstCheckInTime(estCheckInTime)
+                .build();
+    }
+}

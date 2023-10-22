757647ad49e70109c14fa05a3d337801e6d9a842
KYRA PETERSEN <102528904+KYRA219@users.noreply.github.com>
2023-10-22 22:59:10 +0200

    Update ReservationFactoryTest.java

--- src/test/java/za/ac/cput/factory/ReservationFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ReservationFactoryTest.java
@@ -8,8 +8,7 @@ Date : 5 April 2023
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
-import za.ac.cput.domain.Reservation;
-import za.ac.cput.domain.ReservationDate;
+import za.ac.cput.domain.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,8 +20,12 @@ class ReservationFactoryTest {
     @Test
 
     public void createReservation(){
-       ReservationDate reservationDate = ReservationDateFactory.buildReservationDate(LocalDate.of(2023, 9 , 1),  LocalDate.of(2023,9,4),     LocalTime.of(15, 30));
-        Reservation reservation = ReservationFactory.buildReservation( LocalDateTime.now(), "Booking confirmed" , true, false, reservationDate);
+        ReservationDate reservationDate = ReservationDateFactory.buildReservationDate(LocalDate.of(2023, 9 , 1),  LocalDate.of(2023,9,4),     LocalTime.of(15, 30));
+        Guest guest = GuestFactory.createGuest();
+        //Member member = MemberFactory.buildMember("ffloppy@gmail.com", "carrot");
+        Review review = ReviewFactory.buildReview(4, "This was a very pleasant stay. Would recommend");
+        HotelLocation hotelLocation= HotelLocationFactory.createHotelLocation("32 Burger Street Camps Bay", "Cape Town", "Western Cape", 7301, "0215900310");
+        Reservation reservation = ReservationFactory.buildReservation( LocalDateTime.now(), "Booking confirmed" , true, false, reservationDate,guest, hotelLocation/*,member*/, review);
         assertNotNull(reservation);
         System.out.println(reservation);
     }
